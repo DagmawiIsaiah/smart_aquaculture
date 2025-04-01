9fbfd324cca8e0cca2c7d7d6f8b2dd2943591488
duckye <161498434+DagmawiIsaiah@users.noreply.github.com>
2025-04-02 02:27:32 +0300

    create const spacing

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,15 +2,24 @@ import 'dart:ui';
 
 // Colors
 const Color pBlue = Color(0xFF007AFF);
-Color pBlue30 = Color(0xFF007AFF).withAlpha((255*0.3).toInt());
+Color pBlue30 = Color(0xFF007AFF).withAlpha((255 * 0.3).toInt());
 
 const Color red = Color(0xFFFF3B30);
-Color red30 = Color(0xFFFF3B30).withAlpha((255*0.3).toInt());
+Color red30 = Color(0xFFFF3B30).withAlpha((255 * 0.3).toInt());
 
 const Color orange = Color(0xFFFF9500);
 const Color green = Color(0xFF34C759);
 
 const Color black = Color(0xFF000000);
-Color black70 = Color(0xFF000000).withAlpha((255*0.7).toInt());
-Color grey30 = Color(0xFFCDCDCD).withAlpha((255*0.3).toInt());
+Color black70 = Color(0xFF000000).withAlpha((255 * 0.7).toInt());
+Color grey30 = Color(0xFFCDCDCD).withAlpha((255 * 0.3).toInt());
 const Color white = Color(0xFFFFFFFF);
+
+// Spacing
+const double space4 = 4;
+const double space8 = 8;
+const double space12 = 12;
+const double space16 = 16;
+const double space24 = 24;
+const double space32 = 32;
+const double space40 = 46;
