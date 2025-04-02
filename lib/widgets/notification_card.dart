a8d7371961dfa48947b0954fa7772889b5e0435a
duckye <161498434+DagmawiIsaiah@users.noreply.github.com>
2025-04-02 07:04:50 +0300

    create dialog for notification

--- lib/widgets/notification_card.dart
+++ lib/widgets/notification_card.dart
@@ -43,50 +43,105 @@ class NotificationCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        border: Border(
-          left: BorderSide(
-            color: (type == NotificationType.warning) ? pBlue : red,
-            width: 5,
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) => Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            backgroundColor: white,
+            child: Container(
+              constraints: BoxConstraints(maxWidth: 550),
+              padding: EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    children: [
+                      Text(title, style: Theme.of(context).textTheme.bodyLarge),
+                      SizedBox(width: space16),
+                      Text(
+                        "13 Nov, 2024",
+                        style: Theme.of(context)
+                            .textTheme
+                            .labelMedium!
+                            .copyWith(color: black70),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: space12),
+                  Text(
+                    detail,
+                    style: Theme.of(context)
+                        .textTheme
+                        .labelMedium!
+                        .copyWith(color: black70),
+                  ),
+                  SizedBox(height: space24),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text("Okey"),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          border: Border(
+            left: BorderSide(
+              color: (type == NotificationType.warning) ? pBlue : red,
+              width: 5,
+            ),
+          ),
+          color: white,
         ),
-        color: white,
-      ),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Text(title, style: Theme.of(context).textTheme.bodyLarge),
-              SizedBox(width: space16),
-              Text(
-                "13 Nov, 2024",
-                style: Theme.of(context)
-                    .textTheme
-                    .labelMedium!
-                    .copyWith(color: black70),
-              ),
-              Spacer(),
-              CircleAvatar(
-                radius: 4,
-                backgroundColor:
-                    (type == NotificationType.warning) ? pBlue : red,
-              ),
-            ],
-          ),
-          SizedBox(height: space8),
-          Text(
-            detail,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: Theme.of(context)
-                .textTheme
-                .labelMedium!
-                .copyWith(color: black70),
-          ),
-        ],
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(title, style: Theme.of(context).textTheme.bodyLarge),
+                SizedBox(width: space16),
+                Text(
+                  "13 Nov, 2024",
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelMedium!
+                      .copyWith(color: black70),
+                ),
+                Spacer(),
+                CircleAvatar(
+                  radius: 4,
+                  backgroundColor:
+                      (type == NotificationType.warning) ? pBlue : red,
+                ),
+              ],
+            ),
+            SizedBox(height: space8),
+            Text(
+              detail,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context)
+                  .textTheme
+                  .labelMedium!
+                  .copyWith(color: black70),
+            ),
+          ],
+        ),
       ),
     );
   }
