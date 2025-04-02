f62859806ef425b4c90762cb3f5295a76220f0c4
duckye <161498434+DagmawiIsaiah@users.noreply.github.com>
2025-04-02 05:09:47 +0300

    create user card widget

---
+++ lib/widgets/user_card.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+
+import '../utils/utils.dart';
+
+class UserCard extends StatelessWidget {
+  const UserCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: pBlue.withAlpha((255 * 0.05).toInt()),
+      ),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 24,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(1000),
+              child: Image.asset(
+                "images/image1.jpg",
+                height: 48,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(width: space12),
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Dagmawi Esayas",
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                Text(
+                  "SA69813",
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelMedium!
+                      .copyWith(color: black70),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: space8),
+          Expanded(
+            child: GestureDetector(
+              onTap: () {},
+              child: CircleAvatar(
+                backgroundColor: white,
+                child: Image(
+                  width: 32,
+                  height: 32,
+                  color: red,
+                  image: Svg('icons/SignOut.svg'),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
